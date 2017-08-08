e07b4713792d3e89a3d5d856d9bd5b6f9aa63ce5
Autumn Tan <autumn.tan.1@gmail.com>
2017-08-08 14:39:52 -0700

    changed from mm:ss to m minutes and s sectonds, able to stop countdown from being negative

--- timer.js
+++ timer.js
@@ -1,22 +1,41 @@
 window.onload = function()
 {
   var input = prompt("How many minutes would you like to focus for? ")
-   var hou = parseInt(input) - 1;
-   var sec = 59;
-   setInterval(function(){
+  var hou = parseInt(input) - 1;
+  var sec = 59;
+  setInterval(function()
+  {
+    if (hou == 00 && sec == 00)
+    {
+      document.getElementById("timer").innerHTML = "Congratulations! You have worked for " + input + "minutes. Keep it up! Take a short break and come back for another focus session!"
+    }
+    else
+    {
+      // if statement for declaration of variable for
+      if (hou == 0)
+      {
+        var min_display = " "
+      }
+      else if (hou == 1)
+      {
+        var min_display = hou + "minute and "
+      }
+      else
+      {
+        var min_display = hou + " minutes and "
+      }
 
-   document.getElementById("timer").innerHTML =
-   (hou).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) +" : " +
-   (sec).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + " minutes" ;
-   sec--;
-   if (hou == 00 && sec == 00)
-   {
-     location.reload();
-   }
-   if (sec == -1)
-   {
-     hou--;
-     sec = 59;
-   }
+      //variable to hold display for seconds
+      var sec_display = (sec).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false}) + " seconds."
+
+      document.getElementById("timer").innerHTML = "Try to stay on task for " + min_display + sec_display;
+      sec--;
+
+      if (sec == -1)
+      {
+        hou--;
+        sec = 59;
+      }
+    }
   },1000);
 }
