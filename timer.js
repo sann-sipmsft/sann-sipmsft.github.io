a9183b64158fe92cc08855ff7ac7cdacb04eb5ae
Autumn Tan <autumn.tan.1@gmail.com>
2017-08-09 15:38:50 -0700

    added working stop and restart button. still some bugs with the start button

--- timer.js
+++ timer.js
@@ -1,11 +1,14 @@
 function startTimer()
 {
-  var input = prompt("How many minutes would you like to focus for? ")
+  
+  var input = prompt("How many minutes would you like to focus for? ");
   var hou = parseInt(input) - 1;
   var sec = 59;
-  setInterval(function()
+  var intervaltimer = 1000;
+
+  interval1 = setInterval(function()
   {
-    if (parseInt(input) == "NaN")
+    if (parseInt(input) == undefined)
     {
       prompt("Please enter a numeric value.");
       location.reload();
@@ -15,46 +18,62 @@ function startTimer()
       // minute or minutes,based on the original input
       if (input == "1")
       {
-        var min = " minute."
+        var min = " minute.";
       }
       else
       {
-        var min = " minutes."
+        var min = " minutes.";
       }
 
-      //output after timer is done
+        //output after timer is done
       document.getElementById("timer").innerHTML = "Congratulations! You have worked for " + input + min + " Keep it up!<br>Take a short break and come back for another focus session!<br><br>"
       document.getElementById("timerformat").innerHTML = "GOOD JOB!";
+      var elem = document.getElementById('starter');
+      elem.parentNode.removeChild(elem)
+      clearInterval(interval1);
     }
     else
     {
-      // if statement for declaration of variable for
-      if (hou == 0)
-      {
-        var min_display = " "
-      }
-      else if (hou == 1)
-      {
-        var min_display = hou + " minute and "
-      }
-      else
-      {
-        var min_display = hou + " minutes and "
-      }
-
-      //variable to hold display for seconds
-      var sec_display = sec + " seconds"
-
-      // output/ countdown on screen
-      document.getElementById("timer").innerHTML = "TRY TO STAY ON TASK FOR<br><br>";
-      document.getElementById("timerformat").innerHTML = min_display + sec_display;
-      sec--;
-
       if (sec == -1)
       {
         hou--;
         sec = 59;
       }
+        // if statement for declaration of variable for
+      if (hou == 0)
+      {
+        var min_display = " ";
+      }
+      else if (hou == 1)
+      {
+        var min_display = hou + " minute and ";
+      }
+      else
+      {
+        var min_display = hou + " minutes and ";
+      }
+      //variable to hold display for seconds
+      var sec_display = sec + " seconds";
+
+        // output/ countdown on screen
+      document.getElementById("timer").innerHTML = "TRY TO STAY ON TASK FOR<br><br>";
+      document.getElementById("timerformat").innerHTML = min_display + sec_display;
+
+      document.getElementById("restarter").value = "RESTART";
+      sec--;
+
     }
-  },1000);
+  },intervaltimer);
+}
+
+function pauseTimer()
+{
+  clearInterval(interval1);
+  document.getElementById("timerformat").innerHTML = "TIMER HAS BEEN STOPPED. <br> RESTART THE TIMER WHEN YOU ARE READY.";
+}
+
+function restartTimer()
+{
+  clearInterval(interval1);
+  startTimer();
 }
